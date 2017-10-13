c1a73bbc1115a2333cf5be65a59def0d26ac1539
Andrea-Arguello <31134106+Andrea-Arguello@users.noreply.github.com>
2017-10-13 09:02:25 -0600

    Add files via upload

---
+++ Timbre.java
@@ -0,0 +1,27 @@
+/**
+* Timbre.java
+* Andrea Arguello 17801
+13/10/2017
+*/
+
+/** Clase Madre
+*/
+public class Timbre {
+	/**Imprime timbre 1
+	*/
+	public void m1() {
+		System.out.println("timbre 1");
+	}
+	
+	/**Imprime timbre 2017
+	*/
+	public void m2() {
+		System.out.println("timbre 2");
+	}
+
+	/**Imprime Ring...Ring
+	*/
+	public String toString() {
+		return "Ring...Ring";
+	}
+}
